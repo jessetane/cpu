315d7bd294b0fc37de07198eb071584c4145454c
Jesse Tane <jesse.tane@gmail.com>
2013-04-09 22:15:19 -0400

    use env vars

--- package.sh
+++ package.sh
@@ -12,8 +12,7 @@ version() {
 }
 
 repository() {
-  #echo "https://github.com/jessetane/cpu"
-  echo "file:///Users/jessetane/Dropbox/software/src/cpu"
+  echo "https://github.com/jessetane/cpu"
 }
 
 dependencies() {
@@ -25,12 +24,14 @@ fetch() {
 }
 
 update() {
+  cd "$SRC"
   git fetch --all
   git fetch --tags
 }
 
 build() {
   mkdir -p "$LIB"
-  cp -R "$SRC"/.git "$LIB"/
-  cd "$LIB" && git reset --hard "$VERSION"
+  cp -R "$SRC"/.git "$LIB"
+  cd "$LIB"
+  git reset --hard "$VERSION"
 }
